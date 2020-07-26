3a9965744980ff49377c9a4b6452da0153ec2518
bryanzanr <bryanzanr@gmail.com>
2020-07-26 15:49:01 +0700

    successfully predict first try with json first

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import coremltools
 import csv
+import json
 
 def predict_result(result, arr):
     if result['label'] == "worst":
@@ -20,59 +21,69 @@ def main():
     arr['rating'] = []
 
     # with open('test.csv', newline='') as csvfile:
-    with open('test.csv') as f:
-        spamreader = f.readlines()
+    # with open('test.csv') as f:
+    with open('test.json') as file:
+        tests = json.load(file)
         model = coremltools.models.MLModel('SentimentClassifier.mlmodel')
-        # spamreader = csv.reader(csvfile, delimiter='', quotechar='|')
-        text = ""
-        counter = 1
-        for row in spamreader:
-            # temp = row[0].split(",")
-            temp = row.split(",")
-            if temp[0] == 'review_id':
-                arr['review_id'].append("review_id")
-                arr['rating'].append("rating")
-                continue
-            else:
-                temp = row.split(str(counter) + ",")
-            # print(temp)
-            # tmp = next(spamreader).split(",")
-            # if len(tmp) < 2:
-            if len(temp) < 2:
-                text += row
-                continue
-            else:
-                if '"' in temp[1]:
-                #     # print(tmp)
-                    arr['review_id'].append(temp[0])
-                    counter += 1
-                    continue
-                #     temp = row.split('"')
-                #     result = model.predict({'text': text})
-                #     text = ""
-                # else:
-                if text != "":
-                    print(text)
-                    result = model.predict({'text': text})
-                    predict_result(result, arr)
-                    text = ""
-                print(temp[1])
-                result = model.predict({'text': temp[1]})
-                predict_result(result, arr)
-                arr['review_id'].append(counter)
-                counter += 1
-                if counter == 19:
-                    break
+        arr['review_id'].append('review_id')
+        arr['rating'].append('rating')
+        for test in tests:
+            print(test['review_id'])
+            arr['review_id'].append(test['review_id'])
+            result = model.predict({'text': test['review']})
+            predict_result(result, arr)
+            # break
+        # spamreader = f.readlines()
+        # # spamreader = csv.reader(csvfile, delimiter='', quotechar='|')
+        # text = ""
+        # counter = 1
+        # for row in spamreader:
+        #     # temp = row[0].split(",")
+        #     temp = row.split(",")
+        #     if temp[0] == 'review_id':
+        #         arr['review_id'].append("review_id")
+        #         arr['rating'].append("rating")
+        #         continue
+        #     else:
+        #         temp = row.split(str(counter) + ",")
+        #     # print(temp)
+        #     # tmp = next(spamreader).split(",")
+        #     # if len(tmp) < 2:
+        #     if len(temp) < 2:
+        #         text += row
+        #         continue
+        #     else:
+        #         if '"' in temp[1]:
+        #         #     # print(tmp)
+        #             arr['review_id'].append(temp[0])
+        #             counter += 1
+        #             continue
+        #         #     temp = row.split('"')
+        #         #     result = model.predict({'text': text})
+        #         #     text = ""
+        #         # else:
+        #         if text != "":
+        #             print(text)
+        #             result = model.predict({'text': text})
+        #             predict_result(result, arr)
+        #             text = ""
+        #         print(temp[1])
+        #         result = model.predict({'text': temp[1]})
+        #         predict_result(result, arr)
+        #         arr['review_id'].append(counter)
+        #         counter += 1
+        #         if counter == 19:
+        #             break
             # print(temp[0])
             # break
 
-    print(arr)
+    # print(arr)
 
-    # with open('sampleSubmission.csv', mode='w') as test_file:
-    #     test_writer = csv.writer(test_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+    with open('sampleSubmission.csv', mode='w') as test_file:
+        test_writer = csv.writer(test_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
 
-    #     for i in range(len(arr['review_id'])):
-    #         test_writer.writerow([arr['review_id'][i], arr['rating'][i]])
+        for i in range(len(arr['review_id'])):
+            test_writer.writerow([arr['review_id'][i], arr['rating'][i]])
 
 if __name__ == "__main__":
     main()
